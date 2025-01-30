fe8d4869b3c2d55a203a9906db74ca524cb65624
Allen Houchins <allenhouchins@mac.com>
2025-01-29 19:25:08 -0600

    Update macos_version_policy_updater.sh

--- .github/scripts/macos_version_policy_updater.sh
+++ .github/scripts/macos_version_policy_updater.sh
@@ -67,7 +67,7 @@ if [ "$version_number" != "$highest_version" ]; then
                  --arg base "$BRANCH" \
                  '{title: $title, head: $head, base: $base}')
 
-    curl -s -H "Authorization: token $GITHUB_TOKEN" \
+    curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
          -H "Accept: application/vnd.github.v3+json" \
          -X POST \
          -d "$pr_data" \
